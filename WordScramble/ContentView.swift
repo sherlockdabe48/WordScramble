9e44a6456c6ae59b914014144340db1ae490b4f9
sherlockdabe48 <sherlockdabe48@gmail.com>
2021-06-23 11:42:50 +0700

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Antarcticaman on 23/6/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
